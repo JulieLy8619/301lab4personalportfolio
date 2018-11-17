32d5c66c4f75453d3ddc84a5d761051e4c10ca23
JulieLy8619 <july8619@yahoo.com>
2018-11-16 17:13:35 -0800

    depoyed to server

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//load express
+const express = require('express');
+
+// instantiate express so we can use it
+const app = express();
+
+//designate a port to serve our app on
+const PORT = process.env.PORT || 3000;
+
+//define which directory we will serve files from
+app.use(express.static('./public'));
+
+//tell the app to listen
+app.listen(PORT, () => console.log(`Our app is listening on port ${PORT}`));
