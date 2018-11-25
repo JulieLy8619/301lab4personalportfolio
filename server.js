89dd0f56a55eb91283b5e95ba9df6d9c2fe1a7d2
JulieLy8619 <july8619@yahoo.com>
2018-11-24 17:23:16 -0800

    got a carousel on my page but i need it to move down

--- server.js
+++ server.js
@@ -15,31 +15,4 @@ app.use(express.static('./public'));
 //tell the app to listen
 app.listen(PORT, () => console.log(`Our app is listening on port ${PORT}`));
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
